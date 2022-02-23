297bacde4e01178e7afee05c9cd3646f80662858
Tredicibrando <nicolosigiov@gmail.com>
2022-02-23 16:08:33 +0100

    esercizio finito

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('ciao')
+
+const app = new Vue({
+    el: '#sezione',
+    data: {
+        message: 'messaggio stampato da vue',
+        image: 'https://picsum.photos/200/300',
+    }
+});
+
+{{}}
